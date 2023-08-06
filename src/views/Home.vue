29c66a4f9417632b1a241165642ef0e86853edcb
Benedict Olebu <cgolebu.dev@gmail.com>
2023-08-05 23:37:47 -0700

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "Home",
+  setup() {
+    const posts = ref([
+      { title: "welcome to the blog", body: "Lorem ipsum", id: 1 },
+      { title: "welcome to the blog", body: "Lorem ipsum", id: 1 },
+    ]);
+    return { posts };
+  },
+};
+</script>
